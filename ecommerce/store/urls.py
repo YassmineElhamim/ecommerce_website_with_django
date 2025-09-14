2a1a8cf6d400afeb500fb62c38d312ea997be8d8
YassmineElhamim <yassmine9461@gmail.com>
2025-09-14 05:48:55 +0100

    login and logout logic

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('/products', views.products, name='products'),
+    path('products/', views.products, name='products'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
 ]
